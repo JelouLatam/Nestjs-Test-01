1fa964be1714c045383656e823ba90dfe1e4c1bb
Alex Ferrín <alex-710@hotmail.com>
2024-01-18 17:41:16 -0500

    Executed npm run lint and npm run format commands

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -4,18 +4,27 @@ import { NoWhitespaceString } from 'src/validators/NoWhitespaceString';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDto {
-  @ApiProperty({ description: 'Title of the task. Should be a non-empty string, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Title of the task. Should be a non-empty string, otherwise is not a valid value',
+  })
   @IsNotEmpty()
   @MaxLength(30)
   @Validate(NoWhitespaceString)
   title: string;
 
-  @ApiProperty({ description: 'Description of the task. Should be a non-empty string, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Description of the task. Should be a non-empty string, otherwise is not a valid value',
+  })
   @IsNotEmpty()
   @Validate(NoWhitespaceString)
   description: string;
 
-  @ApiProperty({ description: 'Date when the task must be completed. Should be a date string with the following format: yyyy-mm-dd, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Date when the task must be completed. Should be a date string with the following format: yyyy-mm-dd, otherwise is not a valid value',
+  })
   @IsNotEmpty()
   @IsDateString()
   @Validate(NoPreviousDates)
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -6,18 +6,27 @@ import { NoWhitespaceString } from 'src/validators/NoWhitespaceString';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @ApiProperty({ description: 'Title of the task. If provided, it should be a non-empty string, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Title of the task. If provided, it should be a non-empty string, otherwise is not a valid value',
+  })
   @IsNotEmpty()
   @MaxLength(30)
   @Validate(NoWhitespaceString)
   title: string;
 
-  @ApiProperty({ description: 'Description of the task. If provided, it should be a non-empty string, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Description of the task. If provided, it should be a non-empty string, otherwise is not a valid value',
+  })
   @IsNotEmpty()
   @Validate(NoWhitespaceString)
   description: string;
 
-  @ApiProperty({ description: 'Date when the task must be completed. If provided, it should be a date string with the following format: yyyy-mm-dd, otherwise is not a valid value' })
+  @ApiProperty({
+    description:
+      'Date when the task must be completed. If provided, it should be a date string with the following format: yyyy-mm-dd, otherwise is not a valid value',
+  })
   @IsDateString()
   @Validate(NoPreviousDates)
   dueDate: Date;
