104e0b0dcd1f46f5d0b4bb4679b4fafe467e57bf
matardy <gutemberg.mendoza@epn.edu.ec>
2024-04-16 17:50:55 -0500

    update(docs): add swagger doc and ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('This API handles the creation of tasks')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
